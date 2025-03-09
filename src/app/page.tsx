337262e8d926bb3006912e7a42a53eadf353f6fd
thenishantgiri <nishantkrgiri@gmail.com>
2025-03-10 01:22:22 +0530

    Shadcn UI: components added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <h1>Hello World!</h1>
-      <Button variant={"secondary"} size={"sm"}>
-        Click me!
-      </Button>
+      <Button>Primary</Button>
     </div>
   );
 }
